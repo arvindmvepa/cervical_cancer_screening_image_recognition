494f002ab7323af229ba8abadae95236aed9e3b7
arvindmvepa <arvind.m.vepa@gmail.com>
2017-05-27 14:26:47 -0700

    updated file

--- images/tempCNN.py
+++ images/tempCNN.py
@@ -25,27 +25,9 @@ from tflearn.layers.estimator import regression
 
 dataset_file = r'/home/ubuntu/cs249_final_project/image_files/train'
 
-# Build a HDF5 dataset (only required once)
 from tflearn.data_utils import image_preloader
-#build_hdf5_image_dataset(dataset_file, image_shape=(128, 128), mode='folder', output_path='dataset.h5', categorical_labels=True, normalize=True)
 X,Y = image_preloader(dataset_file, image_shape=(128, 128), mode='folder', categorical_labels=True, normalize=True)
 
-# Load HDF5 dataset
-"""
-import h5py
-h5f = h5py.File(r'dataset.h5', 'r')
-X = h5f['X']
-Y = h5f['Y']
-"""
-
-# Build neural network and train
-"""
-network = ...
-model = DNN(network, ...)
-model.fit(X, Y)
-"""
-# Building 'AlexNet'
-"""
 network = input_data(shape=[None, 227, 227, 3])
 network = conv_2d(network, 96, 11, strides=4, activation='relu')
 network = max_pool_2d(network, 3, strides=2)
@@ -73,4 +55,3 @@ model = tflearn.DNN(network, checkpoint_path='model_alexnet',
 model.fit(X, Y, n_epoch=1000, validation_set=0.1, shuffle=True,
           show_metric=True, batch_size=64, snapshot_step=200,
           snapshot_epoch=False, run_id='alexnet_oxflowers17')
-"""
