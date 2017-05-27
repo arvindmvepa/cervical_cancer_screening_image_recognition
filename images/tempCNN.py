bdfa69fe6e00dee10f58ea303dbb4ca2d6843d16
arvindmvepa <arvind.m.vepa@gmail.com>
2017-05-27 14:15:45 -0700

    updated file for image loading

--- images/tempCNN.py
+++ images/tempCNN.py
@@ -27,13 +27,16 @@ dataset_file = r'/home/ubuntu/cs249_final_project/image_files/train'
 
 # Build a HDF5 dataset (only required once)
 from tflearn.data_utils import build_hdf5_image_dataset
-build_hdf5_image_dataset(dataset_file, image_shape=(128, 128), mode='folder', output_path='dataset.h5', categorical_labels=True, normalize=True)
+#build_hdf5_image_dataset(dataset_file, image_shape=(128, 128), mode='folder', output_path='dataset.h5', categorical_labels=True, normalize=True)
+X,Y = image_preloader(dataset_file, image_shape=(128, 128), mode='folder', categorical_labels=True, normalize=True)
 
 # Load HDF5 dataset
+"""
 import h5py
 h5f = h5py.File(r'dataset.h5', 'r')
 X = h5f['X']
 Y = h5f['Y']
+"""
 
 # Build neural network and train
 """
