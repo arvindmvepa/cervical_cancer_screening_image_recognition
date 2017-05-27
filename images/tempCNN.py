bbbdfd813880def951e2768ba9001009ec672fee
arvindmvepa <arvind.m.vepa@gmail.com>
2017-05-27 14:16:46 -0700

    update

--- images/tempCNN.py
+++ images/tempCNN.py
@@ -26,7 +26,7 @@ from tflearn.layers.estimator import regression
 dataset_file = r'/home/ubuntu/cs249_final_project/image_files/train'
 
 # Build a HDF5 dataset (only required once)
-from tflearn.data_utils import build_hdf5_image_dataset
+from tflearn.data_utils import image_preloader
 #build_hdf5_image_dataset(dataset_file, image_shape=(128, 128), mode='folder', output_path='dataset.h5', categorical_labels=True, normalize=True)
 X,Y = image_preloader(dataset_file, image_shape=(128, 128), mode='folder', categorical_labels=True, normalize=True)
 
