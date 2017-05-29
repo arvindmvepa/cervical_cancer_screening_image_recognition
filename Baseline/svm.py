178a6eb11788604a5bf605d3fc885990d6a20080
wwxwwx <twinkletwinklex@Gmail.com>
2017-05-29 09:55:08 -0700

    Update svm.py

--- Baseline/svm.py
+++ Baseline/svm.py
@@ -2,19 +2,16 @@
 from sklearn import svm
 import pandas as pd
 from sklearn.decomposition import PCA
-from sklearn import svm
 import time
 import os
 import numpy as np
-from sklearn import metrics
-from sklearn.svm import SVC
 
-dataset_file = r'/Users/watermelon/Desktop/Courses/CS249Sun/prj/data/small'
+dataset_file = r'/Users/watermelon/Desktop/Courses/CS249Sun/prj/data/train'
 #dataset_file = '/home/ubuntu/small'
 from tflearn.data_utils import image_preloader
 data = []
 label = []
-
+print("Loading images...")
 X,Y = image_preloader(dataset_file, image_shape=(300, 300), mode='folder', categorical_labels=True, normalize=True)
 for i in range(X.__len__()):
     data.append(X.__getitem__(i).reshape(270000,1))
@@ -24,12 +21,26 @@ data = np.array(data)
 data=data.reshape(data.shape[0],data.shape[1])
 label = np.array(label)
 
+#save train_data
+# print("Saving data...")
+# np.save('data.npy',data)
+# np.save('label.npy',label)
 
+# LOAD train_data
+# data=np.load('data.npy')
+# label=np.load('label.npy')
+
+print("Initializing SVM...")
+from sklearn.svm import SVC
 #TODO different kernel
 #clf = SVC(kernel='precomputed')
 clf = SVC()
 clf.fit(data, label)
 
+print("Doing cross validation...")
+from sklearn import metrics
+
+from sklearn.model_selection import cross_val_score
 # 5-fold cross validation
-cross_validation.cross_val_score(clf,data,label,cv=5)
-print (clf.score(data,label))
+scores = cross_val_score(clf, data,label,cv=5)
+print(scores)
