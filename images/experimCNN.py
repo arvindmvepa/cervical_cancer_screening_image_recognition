01a3297f580868329ea153650413f1368513f1f1
arvindmvepa <arvind.m.vepa@gmail.com>
2017-06-03 20:16:43 -0700

    experimental CNN

---
+++ images/experimCNN.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+""" AlexNet.
+
+Applying 'Alexnet' to Oxford's 17 Category Flower Dataset classification task.
+
+References:
+    - Alex Krizhevsky, Ilya Sutskever & Geoffrey E. Hinton. ImageNet
+    Classification with Deep Convolutional Neural Networks. NIPS, 2012.
+    - 17 Category Flower Dataset. Maria-Elena Nilsback and Andrew Zisserman.
+
+Links:
+    - [AlexNet Paper](http://papers.nips.cc/paper/4824-imagenet-classification-with-deep-convolutional-neural-networks.pdf)
+    - [Flower Dataset (17)](http://www.robots.ox.ac.uk/~vgg/data/flowers/17/)
+
+"""
+
+from __future__ import division, print_function, absolute_import
+
+import tflearn
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.normalization import local_response_normalization
+from tflearn.layers.estimator import regression
+
+dataset_file = r'/home/ubuntu/cs249_final_project/image_files/train_test'
+
+from tflearn.data_utils import build_hdf5_image_dataset
+build_hdf5_image_dataset(dataset_file, image_shape=(300, 300), mode='folder', output_path='dataset.h5', categorical_labels=True, normalize=True)
+
+# Load HDF5 dataset
+import h5py
+h5f = h5py.File('dataset.h5', 'r')
+X = h5f['X']
+Y = h5f['Y']
+
+
+#from tflearn.data_utils import image_preloader
+#X,Y = image_preloader(dataset_file, image_shape=(300, 300), mode='folder', categorical_labels=True, normalize=True)
+#print(X.__getitem__(0))
+
+network = input_data(shape=[None, 300, 300, 3])
+network = conv_2d(network, 100, 5, strides=2, activation='relu')
+#network = max_pool_2d(network, 3, strides=2)
+network = local_response_normalization(network)
+network = conv_2d(network, 200, 7, activation='relu')
+#network = max_pool_2d(network, 3, strides=2)
+network = local_response_normalization(network)
+network = conv_2d(network, 200, 10, activation='relu')
+network = conv_2d(network, 200, 30, activation='relu')
+network = conv_2d(network, 200, 50, activation='relu')
+#network = max_pool_2d(network, 3, strides=2)
+network = local_response_normalization(network)
+network = fully_connected(network, 4000, activation='tanh')
+network = dropout(network, 0.5)
+network = fully_connected(network, 4000, activation='tanh')
+network = dropout(network, 0.5)
+network = fully_connected(network, 3, activation='softmax')
+network = regression(network, optimizer='momentum',
+                     loss='categorical_crossentropy',
+                     learning_rate=0.001)
+
+# Training
+model = tflearn.DNN(network, checkpoint_path='model_alexnet',
+                    max_checkpoints=1, tensorboard_verbose=2)
+
+model.fit(X, Y, n_epoch=1000, validation_set=0.1, shuffle=True,
+          show_metric=True, batch_size=64, snapshot_step=200,
+          snapshot_epoch=False, run_id='alexnet_oxflowers17')
